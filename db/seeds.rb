97b1890ed1dc977064f8872ceb9dd2bf8e4ab7a0
Rubén Blázquez <35920425+rubenM23@users.noreply.github.com>
2022-12-30 11:41:56 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,34 +2,33 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Create releases person creator
-creator = Person.find_or_create_by(name: 'Juan', birthdate: '23-09-1950')
+creator = Person.find_or_create_by(name: 'Juan', birthdate: '23-09-1990')
 
 #Create receiver releases
-receiver = Person.find_or_create_by(name: 'Maria', birthdate: '23-03-1950')
+receiver = Person.find_or_create_by(name: 'Maria', birthdate: '23-03-2005')
 
 #Create releases
-
+Release.create(
+  creator: creator,
+  receiver: receiver,
+  subject: 'Asunto 1',
+  content: 'Comunicado 1',
+  previous_release: nil)
+  
 [
-  {
-    creator: creator,
-    receiver: receiver,
-    subject: 'Asunto 1',
-    content: 'Comunicado 1',
-    previous_release: nil,
-  },
   {
     creator: creator,
     receiver: receiver,
     subject: 'Asunto 2',
     content: 'Comunicado 2',
-    previous_release: Release.last,
+    previous_release: Release.last
   },
   {
     creator: receiver,
     receiver: creator,
     subject: 'Asunto 3',
     content: 'Comunicado 3',
-    previous_release: nil,
+    previous_release: nil
   }
 ].each do |release|
   Release.find_or_create_by(release)
@@ -39,10 +38,10 @@ end
 
 [
   {
-    release_id: 1,
+    release_id: Release.first,
   },
   {
-    release_id: 1,
+    release_id: Release.first,
   }
 ].each do |attach|
   Attached.create(attach)
